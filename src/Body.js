4f5868035e460b1e8546dfcc6bb0eb66c5c6ab9c
muk772 <mukundmadhav772@gmail.com>
2023-05-31 23:26:44 +0530

    something

--- src/Body.js
+++ src/Body.js
@@ -1,15 +1,62 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
-import { projectData } from "./ProjectsData";
 import { useSelector } from "react-redux";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 const Body = () => {
   const isDark = useSelector((store) => store.darkMode.isDark);
 
-  if (!isDark) {
-    return <div className="bg-slate-800 text-white">hello</div>;
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true });
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
   }
-  return <div>Hello</div>;
+
+  // if (!isDark) {
+  //   return <div className="bg-slate-800 text-white">hello</div>;
+  // }
+  return (
+    <>
+      <div>Hell</div>
+      <div className="justify-center shadow-lg w-1/4 mx-96 my-28 rounded-lg ">
+        <h2 className="font-bold px-2">
+          <p>{transcript}</p>
+        </h2>
+
+        <p>Microphone: {listening ? "on" : "off"}</p>
+
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-5"
+          onClick={startListening}
+          disabled={listening}
+        >
+          {listening ? "Listening..." : "Start Recording"}
+        </button>
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-5"
+          onClick={SpeechRecognition.stopListening}
+        >
+          Stop Listening
+        </button>
+
+        <button
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded m-5"
+          onClick={resetTranscript}
+        >
+          Reset Recording
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Body;
--- src/MySkills.js
+++ src/MySkills.js
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "./Button";
 
 const mySkills = [
-  "REACT",
+  "REACTttt",
   "REDUX",
   "JAVASCRIPT",
   "TAILWIND",
