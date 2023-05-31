c0a14443eb4849bb3a94489f4df2f1c6901e247a
muk772 <mukundmadhav772@gmail.com>
2023-05-31 23:41:40 +0530

    homepage removed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Header";
 
-import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Footer from "./Footer";
-import { Provider, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import store from "../src/Utils/Store";
 
 function App() {
