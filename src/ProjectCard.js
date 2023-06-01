d0b4b26459eea6cbe12dc9be47fb19e4c1545aca
muk772 <mukundmadhav772@gmail.com>
2023-06-02 00:57:05 +0530

    added button to project

--- src/ProjectCard.js
+++ src/ProjectCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ProjectCard = ({ info }) => {
   return (
@@ -9,9 +10,13 @@ const ProjectCard = ({ info }) => {
           alt="project image"
           src={info.projectImg}
         />
-        <ul>
+        <ul className="px-5">
           <li className="font-bold m-2">{info.projectName}</li>
-          {/* <li className="m-2">{info.projectDesc}</li> */}
+          <Link to={info.projectLink} target="_blank">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded px-24">
+              Go To Project
+            </button>
+          </Link>
         </ul>
       </div>
       <div className="mt-5 w-3/4">
